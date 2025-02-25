9fc36b05e27ab196a2aa57ae9e1d9e474deb535b
Eric Siqueira <eric.siqueira005@gmail.com>
2025-02-25 11:19:27 +0530

    Create LifecycleAssessment.py

---
+++ LifecycleAssessment.py
@@ -0,0 +1,9 @@
+'''
+According to a report by France's electricity grid operator RTE, even when the full lifecycle of raw material extraction and processing, equipment manufacturing, installation, and maintenance is taken into account,
+the total CO2 emissions produced by renewable and nuclear generation technologies are much lower than those produced by oil, coal, and gas plants.
+
+As of 2018 India ranks ffh in installed renewable energy capacity with the fourth most
+attractive renewable energy market in the world.
+
+
+'''
